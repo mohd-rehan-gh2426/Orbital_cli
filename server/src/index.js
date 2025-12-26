4ac0f1e44c71d6a3226b04171e674b04fafa854f
Mohd Rehan <mdrehanchhipa@gmail.com>
2025-12-26 10:58:32 +0530

    02 Commit

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = Number(process.env.PORT) || 4000;
+
+app.listen(PORT,() => {
+    console.log(`Server is listening on PORT ${PORT}`);
+})
